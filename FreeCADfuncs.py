fe0743f51e623a6a980786e1f1e5361d25a97ee8
Unknown <blooop@gmail.com>
2017-08-23 19:10:41 +0100

    removed redundant includes

--- FreeCADfuncs.py
+++ FreeCADfuncs.py
@@ -1,14 +1,6 @@
-import sys
-sys.path.append('C:\Program Files\FreeCAD 0.16')
-
-import FreeCAD
-
-App = FreeCAD
-import FreeCADGui
-Gui = FreeCADGui
 import Sketcher
 import Draft, Part
-from FreeCAD import Gui
+
 
 import collections
 
--- point.py
+++ point.py
@@ -1,5 +1,32 @@
 from v2d import *
 
 
+class pt:
+    def __init__(self, pos=None,defineOrigin = False):
+        if not defineOrigin:
+            self.pos = pos or v()
+            self.id = sk().addGeometry(Part.Point(self.pos))
+
+    def origin(self):
+        self.pos = v(0, 0)
+        self.id = -1
+
+    def conPoint(self, pnt):
+        otherid = 1
+        if isinstance(pnt,circle):
+            otherid = 3
+        sk().addConstraint(Sketcher.Constraint('Coincident', self.id, 1, pnt.id, otherid))
+
+    def lineTo(self, otherPoint=None, dis=None):
+        tmp = ln(self, otherPoint, dis)
+        return tmp
+
+    def conVert(self,point):
+        ln(self,point,construction=True).conVert()
+
+    def conDis(self,point,dis):
+        conId = sk().addConstraint(Sketcher.Constraint('Distance',  self.id,1,point.id,1, dis))
+        sk().setDatum(conId,App.Units.Quantity(str(dis) +' mm'))
+
 
 
