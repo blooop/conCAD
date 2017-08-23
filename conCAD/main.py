ef15b58c7057eb67c37b092fb318ed39edb051a5
Unknown <blooop@gmail.com>
2017-08-23 17:07:47 +0100

    removed keysim

---
+++ conCAD/main.py
@@ -0,0 +1,16 @@
+import sys
+sys.path.append('C:\Program Files\FreeCAD 0.16')
+import FreeCAD
+
+App = FreeCAD
+import FreeCADGui
+Gui = FreeCADGui
+import Sketcher
+import Draft, Part
+from FreeCAD import Gui
+import random
+from PySide import QtGui
+import math
+import collections
+
+Sketcher.addConstraint()
