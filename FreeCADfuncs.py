2522d7244168d0f08c47f6b9e5b93d46c9a1ff36
Unknown <blooop@gmail.com>
2017-08-23 18:47:03 +0100

    can import file

--- FreeCADfuncs.py
+++ FreeCADfuncs.py
@@ -4,8 +4,6 @@ def hackyImport(file):
 import sys
 sys.path.append('C:\Program Files\FreeCAD 0.16')
 
-hackyImport('sketchManager.py')
-
 import FreeCAD
 
 App = FreeCAD
@@ -27,16 +25,6 @@ from sketchManager import *
 
 lines = dict()
 loops = []
-
-def createSketchIfNoneExist():
-    FreeCADGui.activateWorkbench("SketcherWorkbench")
-    #if App.activeDocument() is None:
-    #App.newDocument("Unnamed")
-    FreeCAD.setActiveDocument("Unnamed")
-    FreeCAD.activeDocument().addObject('Sketcher::SketchObject', 'Sketch')
-    FreeCADGui.activeDocument().setEdit('Sketch')
-    FreeCADGui.activeDocument().activeView().setCamera('#Inventor V2.1 ascii \n OrthographicCamera {\n viewportMapping ADJUST_CAMERA \n position 0 0 87 \n orientation 0 0 1  0 \n nearDistance -112.88701 \n farDistance 287.28702 \n aspectRatio 1 \n focalDistance 87 \n height 143.52005 }')
-
 
 def randVec(instances=1):
     if instances > 1:
