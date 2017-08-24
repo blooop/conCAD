d5ac4c190a58e00626449216fbe4e156d265434f
Unknown <blooop@gmail.com>
2017-08-24 17:34:44 +0100

    separate files work

--- circleclass.py
+++ circleclass.py
@@ -6,6 +6,37 @@ import Part
 import Sketcher
 
 
+
+class circle:
+    def __init__(self,rad = None,pos=None,construction=False):
+        self.rad = rad
+        self.pos = pos or pt()
+        # sk().addGeometry(Part.Circle(pos,App.Vector(0, 0, 1), rad),construction=construction)
+        self.id = sk().addGeometry(Part.Circle(),construction)
+        self.center = pt()
+        if self.rad is not None:
+            conRad(self.rad,self.id)
+        # if pos is not None:
+
+    def conPoint(self,pnt):
+        sk().addConstraint(Sketcher.Constraint('Coincident',self.id,3,pnt.id,1))
+
+    def conDis(self,obj,dis):
+        if isinstance(obj, pt):
+            sk().addConstraint(Sketcher.Constraint('Distance', obj.id, dis))
+        elif isinstance(obj, ln):
+            sk().addConstraint(Sketcher.Constraint('Distance',self.id,3,obj.id, dis))
+
+    def tangent(self,obj):
+        print "s"
+        if isinstance(obj,ln):
+            sk().addConstraint(Sketcher.Constraint('Tangent', obj.id, self.id))
+        else:
+            raise Exception("tangent must be a curve")
+
+    def conEdgeDis(self,obj,dis):
+        conDis(obj,self.rad+dis)
+
         # pt1 = pt()
         # pt2 = pt(v(1,1))
         # sk().addConstraint(Sketcher.Constraint('Coincident',self.id,3,pt1.id, 1))
